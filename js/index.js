a0f71370e937cefc400230f60b37c420016503b2
oderathegreat <oderathegreat@gmail.com>
2022-01-21 09:33:23 +0300

    added local storage

--- js/index.js
+++ js/index.js
@@ -1,22 +1,30 @@
 let save_Input = document.getElementById('saveInput');
-
 const myLeads = [];
 const inputEL = document.getElementById('inputEl');
-
 const ulEl = document.getElementById('ul-el');
 
 
+//saving user data into local storage
+let leadsStrogae = JSON.parse(localStorage.getItem('myLeads'));
 
+//check if user data is passed
+
+if(leadsStrogae) {
+myLeads = localStorage;
+renderLeads();
+
+}
 
 
 //on click listerner for save Input element
 save_Input.addEventListener('click', function() {
+ //document.getElementById("msg").value = ""; 
+      myLeads.push(inputEL.value);
 
-    //document.getElementById("msg").value = "";
-   
-    myLeads.push(inputEL.value);
-    renderLeads();
-   
+      //local storage of our array items
+localStorage.setItem('myLeads', JSON.stringify(myLeads));
+
+       renderLeads();     
 });
 
 //render leads function
